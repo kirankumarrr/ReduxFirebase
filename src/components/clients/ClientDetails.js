68fc977aa11bd26374968defe154c0250438a8dc
kiran kumar reddy <kirankumarrrreddy@gmail.com>
2018-12-29 05:24:52 -0800

    Edit & Delete inProgress

--- src/components/clients/ClientDetails.js
+++ src/components/clients/ClientDetails.js
@@ -5,14 +5,83 @@ import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import PropTypes from "prop-types";
 import Spinner from "../layouts/Spinner";
+import classnames from "classnames";
 
 class ClientDetails extends Component {
+  state = {
+    showBalanceUpdate: false,
+    balanceUpdateAmount: ""
+  };
   render() {
     const { client } = this.props;
     if (client) {
       return (
         <div>
-          <h1>{client.firstName} </h1>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/" className="btn btn-link">
+                <i className="fas fa-arrow-circle-left">Back to Dashboard</i>
+              </Link>
+            </div>
+            <div className="col-md-6">
+              <Link to={`/client/edit/${client.id}`} className="btn btn-dark">
+                Edit
+              </Link>{" "}
+              <button className="btn btn-danger">Delete</button>
+            </div>
+          </div>
+          <hr />
+          <div className="card">
+            <h3 className="card-header">
+              {client.firstName} {client.lastName}
+            </h3>
+            <div className="card-body">
+              <div className="row">
+                <div className="col-md-8 col-sm-6">
+                  <h3>
+                    Client ID:{" "}
+                    <span className="text-secondary">{client.id}</span>
+                  </h3>
+                </div>
+                <div className="col-md-4 col-sm-6">
+                  <h3 className="pull-right">
+                    Balance:{" "}
+                    <span
+                      className={classnames({
+                        "text-danger": client.balance > 0,
+                        "text-success": parseInt(client.balance) === 0
+                      })}
+                    >
+                      ${parseFloat(client.balance).toFixed(2)}
+                    </span>
+                    <small>
+                      <a
+                        href="#!"
+                        onClick={() =>
+                          this.setState({
+                            showBalanceUpdate: !this.state.showBalanceUpdate
+                          })
+                        }
+                      >
+                        {"  "}
+                        <i className="fas fa-pencil-alt" />
+                      </a>
+                    </small>
+                    {}
+                  </h3>
+                </div>
+              </div>
+              <hr />
+              <ul className="list-gruop">
+                <li className="list-group-item">
+                  Contact Email: {client.email}
+                </li>
+                <li className="list-group-item">
+                  Contact Phone : {client.phone}
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       );
     } else {
@@ -20,6 +89,11 @@ class ClientDetails extends Component {
     }
   }
 }
+
+ClientDetails.propTypes = {
+  firestore: PropTypes.object.isRequired
+};
+
 //getting data from clients
 //Here we dont need any customized Redux Events to fetch data
 //collection -- which is client created  a database table in Firebase
@@ -28,6 +102,7 @@ class ClientDetails extends Component {
 // : Rewrite the excat Reason ????
 // doc : Get Id from URL
 // Adding props : The reason is we are this because we wanted ID
+
 export default compose(
   firestoreConnect(props => [
     { collection: "clients", storeAs: "client", doc: props.match.params.id }
@@ -37,3 +112,5 @@ export default compose(
     //ordered.client[0] this is gonna be array -- we wanted first
   }))
 )(ClientDetails);
+
+//Here installing classname beacuse we can handle dynamically : to show  how over div should look
